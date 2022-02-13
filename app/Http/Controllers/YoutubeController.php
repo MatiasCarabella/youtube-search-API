72b2c6582a06f36e9f45c7143455290fb54b43e6
MatiasCarabella <matias.carabella@outlook.com>
2022-02-13 04:11:22 -0300

    Finishing update

--- app/Http/Controllers/YoutubeController.php
+++ app/Http/Controllers/YoutubeController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Exception;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Response;
+use Exception;
 use Throwable;
 
 class YoutubeController extends Controller
@@ -17,14 +16,12 @@ class YoutubeController extends Controller
 
     public function searchByKeyword($keyword)
     {
-
         /**
          * Checking for Google API Key parameter (Optional),
          * If present, we use it.
          * For Demo purposes, if not present we use my personal API Key.
          */
         $apiKey = isset($_GET['api_key']) ? $_GET['api_key'] : self::API_KEY_DEFAULT;
-
         /**
          * Checking for Results per Page parameter (Optional),
          * - Must be numeric
@@ -51,7 +48,7 @@ class YoutubeController extends Controller
         ];
 
         try {
-            $response = Http::get("https://www.googleapis.com/youwtube/v3/search", $queryParams);
+            $response = Http::get("https://www.googleapis.com/youtube/v3/search", $queryParams);
             /**
              * Check whether or not the Response has items,
              * - If yes return it with the corresponding format
@@ -60,52 +57,48 @@ class YoutubeController extends Controller
             return array_key_exists('items', json_decode($response->body(), true)) ? 
                     $this->formatResponse($response) :
                     new Response($response->body(), $response->status(), ['Content-Type' => 'application/json']);
-
         } catch (Exception $e) {
             abort($e->getCode(), $e->getMessage());
-        } catch (Throwable) {
+        } catch (Throwable $t) {
             abort(500);
         }
     }
 
     private function formatResponse($response)
     {
-        // CONVIRTIENDO EL JSON A UN ARRAY TRABAJABLE
-        $responseArray = json_decode($response->body(), true);
-        // CREO EL ARRAY QUE VA A GUARDAR TODOS LOS VIDEOS
-        $formattedVideoArray = Array();
-        foreach($responseArray["items"] as $video)
+        // Convert the raw Response's body into an array
+        $body = json_decode($response->body(), true);
+        // Create the variable that will store all the video's info
+        $videos = Array();
+        // Go through every element of the List, storing the relevant data in the $videos Array
+        foreach($body["items"] as $item)
         {
-            // RECORRO CADA VIDEO DE LA LISTA Y ME GUARDO LA INFO RELEVANTE
-            $formattedVideo = [
-                "published_at" => $video['snippet']['publishedAt'],
-                "id" => $video['id']['videoId'],
-                "title" => $video['snippet']['title'],
-                "description" => $video['snippet']['description'],
-                "thumbnail" => $video['snippet']['thumbnails']['default']['url'],
+            $video = [
+                "published_at" => $item['snippet']['publishedAt'],
+                "id" => $item['id']['videoId'],
+                "title" => $item['snippet']['title'],
+                "description" => $item['snippet']['description'],
+                "thumbnail" => $item['snippet']['thumbnails']['default']['url'],
                 "extra" => [
-                        "direct_link" => "https://www.youtube.com/watch?v=".$video['id']['videoId'],
-                        "channel_title" => $video['snippet']['channelTitle']
+                        "direct_link" => "https://www.youtube.com/watch?v=" . $item['id']['videoId'],
+                        "channel_title" => $item['snippet']['channelTitle']
                 ]
             ];
-            // CARGO C/ ARRAY DE VIDEO EN EL ARRAY DE VIDEOS GENERAL
-            array_push($formattedVideoArray, $formattedVideo);
+            array_push($videos, $video);
         }
-        // ARMO EL ARRAY GENERAL Y LE AGREGO EL ARRAY DE VIDEOS
-        $formattedResponse = [
-            "total_results" => $responseArray['pageInfo']['totalResults'],
-            "results_per_page" => $responseArray['pageInfo']['resultsPerPage'],
-            "videos" => $formattedVideoArray
+
+        $formattedResponse = Array();
+
+        // If the current page is not the first one, the 'prevPageToken' element is displayed
+        $formattedResponse += array_key_exists('prevPageToken', $body) ? ["prev_page_token" => $body['prevPageToken']] : [];
+
+        // If there's more pages, the 'nextPageToken' element is displayed
+        $formattedResponse += array_key_exists('nextPageToken', $body) ? ["next_page_token" => $body['nextPageToken']] : [];
+
+        return $formattedResponse += [
+            "total_results" => $body['pageInfo']['totalResults'],
+            "results_per_page" => $body['pageInfo']['resultsPerPage'],
+            "videos" => $videos
         ];
-        // SI NO ESTAMOS EN LA PRIMERA PÁGINA MUESTRO EL TOKEN DE LA PÁG ANTERIOR
-        if(array_key_exists('prevPageToken', $responseArray)){
-            $formattedResponse = array("prev_page_token" => $responseArray['prevPageToken']) + $formattedResponse;
-        }
-        // SI HAY MÁS PÁGINAS CON ITEMS MUESTRO EL TOKEN DE LA PÁG SIGUIENTE
-        if(array_key_exists('nextPageToken', $responseArray)){
-            $formattedResponse = array("next_page_token" => $responseArray['nextPageToken']) + $formattedResponse;
-        }
-        // FINISH
-        return $formattedResponse;
     }
 }
--- tests/Feature/YoutubeControllerTest.php
+++ tests/Feature/YoutubeControllerTest.php
@@ -6,19 +6,21 @@ use Tests\TestCase;
 
 class YoutubeControllerTest extends TestCase
 {
+    const EXAMPLE_KEYWORD = 'paradise';
+
     // Check for Http Status 200 - OK
-    public function testHttpStatus200(){
-        $exampleKeyword = "test";
-        $this->json('get', '/api/youtubeSearch/'.$exampleKeyword)
+    public function testHttpStatus200() {
+        $this->json('GET', '/api/youtubeSearch/' . self::EXAMPLE_KEYWORD)
          ->assertStatus(200);
     }
     
     // Validating that the Response JSON has the correct structure
-    public function testReturnsDataInValidFormat(){
-        $exampleKeyword = "test";
-        $this->json('get', '/api/youtubeSearch/'.$exampleKeyword)
+    public function testReturnsDataInValidFormat() {
+        $this->json('GET', '/api/youtubeSearch/' . self::EXAMPLE_KEYWORD)
          ->assertJsonStructure(
              [
+                 'total_results',
+                 'results_per_page',
                  'videos' => [
                         '*' => [
                         'published_at',
