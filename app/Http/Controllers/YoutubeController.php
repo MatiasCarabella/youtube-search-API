8aff6d20409048944a5f27ce1e78e40fc32b8d56
MatiasCarabella <matias.carabella@outlook.com>
2022-07-14 00:05:45 -0300

    Various improvements (Project layout, ErrorResponses, Tests)

--- app/Http/Controllers/YoutubeController.php
+++ app/Http/Controllers/YoutubeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Services\YoutubeService;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+use function PHPUnit\Framework\isNull;
 
 class YoutubeController extends Controller
 {
@@ -17,23 +20,26 @@ class YoutubeController extends Controller
         /**
          * Checking for Google API Key parameter (Optional),
          * If present, we use it.
-         * For Demo purposes, if not present we use my personal API Key.
+         * If not present it defaults to the one set in the ENV file.
          */
         $apiKey = $request->header('api_key') !== NULL ? $request->header('api_key') : env('API_KEY_DEFAULT');
 
         // BODY VALIDATION:
         $body = json_decode($request->getContent());
+        if ($body === null || !isset($body->search)) {
+            return $youtubeService->errorResponse(Response::HTTP_BAD_REQUEST, "Field 'search' is mandatory.");
+        }
+
         /**
          * Checking for Results per Page parameter (Optional),
          * - Must be numeric
          * - Must be between 0 and 10
          * If not present or invalid, it defaults to 10.
          */
-        $resultsPerPage = (isset($body->results_per_page) &&
-                        is_numeric($body->results_per_page) &&
-                        $body->results_per_page >= self::RESULTS_MIN &&
-                        $body->results_per_page <= self::RESULTS_MAX) ?
-                            $body->results_per_page : self::RESULTS_DEFAULT;
+        $resultsPerPage = (isset($body->results_per_page) && is_numeric($body->results_per_page) &&
+                        $body->results_per_page >= self::RESULTS_MIN && $body->results_per_page <= self::RESULTS_MAX) ?
+                            $body->results_per_page :
+                            self::RESULTS_DEFAULT;
 
         /**
          * Checking for Page Token parameter (Optional),
